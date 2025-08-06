4f5dcbf6a279f66505f88c14a0c96a22a19aa483
Lenovo <dieter.vanderelst@fastmail.net>
2025-08-06 19:46:40 -0400

    corrected little issue

--- sync_config.py
+++ sync_config.py
@@ -1,11 +1,11 @@
 
 PROJECTS = {
     "3PI_robot": {
-        "origin": "/home/dieter/Desktop/origin",
+        "origin": "/media/dieter/MicroPython",
         "target": "/home/dieter/Dropbox/PythonRepos/3PI_robot/robot_code"
     },
     "SensorBoard": {
-        "origin": "PLACE HOLDER,
+        "origin": "PLACE HOLDER",
         "target": "PLACE HOLDER"
     },
     "CircuitExperiment": {
